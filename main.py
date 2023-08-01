af1d8aaf5982254f3f20aa188ca67d1064157513
JAAR <joe.aaron.acosta@outlook.com>
2023-07-31 21:04:51 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/31/2023
+>>> Practicing Fundamentals Program 13
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that prints a text file to the console and counts the number of lines in the text file as well as the occurrence of each word in the text file.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
